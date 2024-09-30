05ec33afa4a460ed91ef7f4c3db2407f6deb26b6
Gianmaria Fadda <faddagianmaria10@gmail.com>
2024-09-30 10:01:49 +0200

    completata prima parte

--- index.php
+++ index.php
@@ -1,11 +1,85 @@
+<?php
+
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Hotel</title>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
 </head>
 <body>
     
+    <h1>
+        PHP Hotel
+    </h1>
+
+    <div>
+        <?php
+            foreach($hotels as $ind => $hotel) {
+                <ul>
+                    <li>
+                        NAME: <?php echo $hotels[$i]['name']; ?>
+                    </li>
+                    <li>
+                        Description: <?php echo $hotels[$i]['description']; ?>
+                    </li>
+                    <li>
+                        parking: <?php echo $hotels[$i]['parking'] == true ? 'Si' : 'No'; ?>
+                    </li>
+                    <li>
+                        Vote: <?php echo $hotels[$i]['vote']; ?>
+                    </li>
+                    <li>
+                        distance to center: <?php echo $hotels[$i]['distance_to_center']; ?>
+                    </li>
+                </ul>
+
+                <hr>
+            }
+        ?>
+    </div>
+
 </body>
 </html>
